19896943e7052da186f07cf276c113fc94c88699
Tomerosh <tomerosh@gmail.com>
2026-07-20 12:05:16 +0300

    Router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
+import Layout from '../pages/Layout'
+import Home from '../pages/Home'
+import NotFound from '../pages/NotFound'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    children: [
+      {index:true, element:<Home/>},
+      // {path:'search', element:<Search/>},
+      // {path:'fav', element:<Favorites/>},
+      // {path: "user", element: <User/>, children:[
+        // {path:'login', element:<Login/>},
+        // {path:'register', element:<Register/>},
+        // {path:'profile', element:<Profile/>}
+      // ]},
+      {path:'*', element:<NotFound/>},
+    ]
+  }])
 
 function App() {
-
-  return (
-    <>
-    </>
-  )
+  
+  return <RouterProvider router={router}/>
 }
 
 export default App
